69ec2bc4ff993f2c4d6ca184c70db9ca482a3ba9
TSA <tsasoft7@gmail.com>
2025-03-13 17:44:15 +0530

    13-03-2025

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const port = process.env.port
+const port = process.env.port || 8080
 
 app.get('/', (req, res) => res.status(200).send('The API TSA laboratory API is working🎉'))
 app.listen(port, ()=> console.log(`The server listen at http://localhost:${port}`))
